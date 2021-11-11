52ab9428c1b3aabfe3e2d7665cbd4bfdfa495334
evasivefox <34774459+evasivefox@users.noreply.github.com>
2021-11-11 15:20:40 +0000

    Derp

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl; // I can print hello world, so you know I'm a serious programmer now.
+
+    return EXIT_SUCCESS;
+}
